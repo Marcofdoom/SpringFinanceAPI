7f0f23469befba8541e5968aa2e752c1faff538b
Marcofdoom <marcvpartington@gmail.com>
2019-08-15 09:46:55 +0100

    first commit

---
+++ src/main/java/com/bae/anprapi/SpringFinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.bae.anprapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFinanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFinanceApiApplication.class, args);
+	}
+
+}
